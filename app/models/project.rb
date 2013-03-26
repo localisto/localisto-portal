4bab013b19ee21ca5bcf3d13aca06604c9c9bc2f
Dustin McBride <dustin@chivve.com>
2013-03-26 15:58:02 -0700

    added aws support for photo cropping

--- app/models/project.rb
+++ app/models/project.rb
@@ -23,16 +23,16 @@ class Project < ActiveRecord::Base
 
 
   has_attached_file :image,
-    #:storage => 's3',
-    #:s3_credentials => "config/config.yml",
-    #:bucket => 'localisto-app',
-    #:s3_host_alias => 'cdn.localisto.org',
-    #:url => ':s3_alias_url',
-    #:path => "/grid_images/:style/:id/:filename",
+    :storage => 's3',
+    :s3_credentials => "config/config.yml",
+    :bucket => 'localisto-app',
+    :s3_host_alias => 'cdn.localisto.org',
+    :url => ':s3_alias_url',
+    :path => "/grid_images/:style/:id/:filename",
     :styles => { :thumb  => '230x246#', :large => '500x500>' },
-    #:default_style => :original,
-    #:default_url => 'http://localisto.org/pixel.gif',
-    #:s3_headers => { 'Expires' => 1.year.from_now.httpdate },
+    :default_style => :original,
+    :default_url => 'http://localisto.org/pixel.gif',
+    :s3_headers => { 'Expires' => 1.year.from_now.httpdate },
     #:convert_options => { :all => '-strip -trim' },
     :processors => [:cropper]
 
@@ -45,7 +45,7 @@ class Project < ActiveRecord::Base
   def image_geometry(style = :original)
     @geometry ||= {}
     #path = (image.options[:storage]==:s3) ? image.url(style) : image.path(style)
-    @geometry[style] ||= Paperclip::Geometry.from_file(image.path(style))
+    @geometry[style] ||= Paperclip::Geometry.from_file("http://cdn.localisto.org" + image.path(style))
     #"http://cdn.localisto.org" + 
   end
 
@@ -81,12 +81,12 @@ class Project < ActiveRecord::Base
 
 
   def update_image_field
-    self.grid_image = self.image.url
+    self.grid_image = self.image.url(:thumb)
     self.save
   end
 
   def update_image_fieldu
-    self.grid_image = self.image.url
+    self.grid_image = self.image.url(:thumb)
 
   end
 
